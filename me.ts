49967f4c6ff79f94c70aeb50074872c7dfcce167
yajamon <yajamon.tatsuki@gmail.com>
2024-02-16 16:57:45 +0900

    Content-Typeも入れてねとあったので

--- me.ts
+++ me.ts
@@ -6,6 +6,7 @@ const api_token = (await Deno.readTextFile("api_token.txt")).trim();
 const res = await fetch(endpoint, {
     method: "GET",
     headers: {
+        "Content-Type": "application/json",
         "Authorization": `Basic ${btoa(`${api_token}:api_token`)}`
     }
 });
