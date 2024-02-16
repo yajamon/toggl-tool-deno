edce492636620d105d4e446ef7c2b7d999568508
yajamon <yajamon.tatsuki@gmail.com>
2024-02-16 16:56:14 +0900

    meを叩いて基本的な操作方法を確認する

---
+++ me.ts
@@ -0,0 +1,14 @@
+const endpoint = "https://api.track.toggl.com/api/v9/me";
+
+// ファイルから読み込む
+const api_token = (await Deno.readTextFile("api_token.txt")).trim();
+
+const res = await fetch(endpoint, {
+    method: "GET",
+    headers: {
+        "Authorization": `Basic ${btoa(`${api_token}:api_token`)}`
+    }
+});
+
+const data = await res.json();
+console.log(data);
